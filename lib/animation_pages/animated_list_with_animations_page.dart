5878ae9f723d51a6dcdc3e91f1d401b9b1e8049d
Sarbad0r <avaz.shams.2002@gmail.com>
2023-06-05 22:17:10 +0500

    uploaded

--- lib/animation_pages/animated_list_with_animations_page.dart
+++ lib/animation_pages/animated_list_with_animations_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_2/animation_pages/neumorphic_container_page.dart';
 import 'package:flutter_animations_2/animations/fade_animation.dart';
 import 'package:flutter_animations_2/animations/scale_animation.dart';
 import 'package:flutter_animations_2/animations/slide_animation.dart';
@@ -9,73 +10,85 @@ class AnimatedListWithAnimationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => NeumorphicContainer())),
+              icon: Icon(Icons.add))
+        ],
+      ),
       body: ListView.separated(
           separatorBuilder: (context, index) => const SizedBox(height: 20),
           itemCount: 10,
           itemBuilder: (context, index) => SlideAnimation(
-            intervalStart: getIntervalStartByIndex(index),
-            duration: const Duration(milliseconds: 1500),
+                intervalStart: getIntervalStartByIndex(index),
+                duration: const Duration(milliseconds: 1500),
                 curve: Curves.bounceOut,
                 begin: Offset(150, 0),
                 child: Card(
-                  color: Colors.amber,
+                    color: Colors.amber,
                     child: Row(children: [
-                  ScaleAnimation(
-                    begin: 0.4,
-                    end: 1,
-                    curve: Curves.elasticInOut,
-                    duration: const Duration(milliseconds: 1500),
-                    intervalStart: getIntervalStartByIndex(index + 3),
-                    child: SlideAnimation(
-                      begin: const Offset(0, 150),
-                      // end: Offset(0,100),
-                      duration: const Duration(milliseconds: 500),
-                      intervalStart: getIntervalStartByIndex(index + 5),
-                      child: SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Image.asset('assets/nfts/${index + 1}.png'),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  Expanded(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                        FadeAnimation(
-                          intervalStart: getIntervalStartByIndex(index),
-                          child: const Text("Gladias Baker",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16)),
+                      ScaleAnimation(
+                        begin: 0.4,
+                        end: 1,
+                        curve: Curves.elasticInOut,
+                        duration: const Duration(milliseconds: 1500),
+                        intervalStart: getIntervalStartByIndex(index + 3),
+                        child: SlideAnimation(
+                          begin: const Offset(0, 150),
+                          // end: Offset(0,100),
+                          duration: const Duration(milliseconds: 500),
+                          intervalStart: getIntervalStartByIndex(index + 5),
+                          child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: Image.asset('assets/nfts/${index + 1}.png'),
+                          ),
                         ),
-                        const SizedBox(height: 10),
-                        SlideAnimation(
-                          intervalStart: getIntervalStartByIndex(index),
-                          child: const Row(
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                          child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Icon(Icons.star, color: Colors.amber, size: 20),
-                                Flexible(
-                                  child: Text("4.3 ",
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16)),
-                                ),
-                                Text("Digital Art ",
-                                    style: TextStyle(color: Colors.black45)),
-                                Flexible(
-                                  child: Text("\$2.2",
-                                      style: TextStyle(color: Colors.black45)),
-                                ),
-                              ]),
-                        )
-                      ]))
-                ])),
+                            FadeAnimation(
+                              intervalStart: getIntervalStartByIndex(index),
+                              child: const Text("Gladias Baker",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16)),
+                            ),
+                            const SizedBox(height: 10),
+                            SlideAnimation(
+                              intervalStart: getIntervalStartByIndex(index),
+                              child: const Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Icon(Icons.star,
+                                        color: Colors.amber, size: 20),
+                                    Flexible(
+                                      child: Text("4.3 ",
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16)),
+                                    ),
+                                    Text("Digital Art ",
+                                        style:
+                                            TextStyle(color: Colors.black45)),
+                                    Flexible(
+                                      child: Text("\$2.2",
+                                          style:
+                                              TextStyle(color: Colors.black45)),
+                                    ),
+                                  ]),
+                            )
+                          ]))
+                    ])),
               )),
     );
   }
