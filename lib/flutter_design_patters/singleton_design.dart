937899024a249d7ec4e6328b158bf7270189aece
Sarbad0r <avaz.shams.2002@gmail.com>
2023-08-06 11:48:50 +0500

    uploaded

--- lib/flutter_design_patters/singleton_design.dart
+++ lib/flutter_design_patters/singleton_design.dart
@@ -2,16 +2,15 @@ import 'package:flutter/cupertino.dart';
 
 //singleton design patters is whenever in class or file you call several instance of object
 //the constructor of this class will work only once.
-//It means that you create only one object of this class in whole project
+//It means that when you create many object of this class, you create only one object of this class in whole project
 class Singleton {
   static Singleton? _instance;
 
-  //we get instance of this class without calling object. Checking if we already
+  //we get instance of this class without calling object. Checking if we already create
   static Singleton? get instance => _instance ??= Singleton._();
 
   //private constructor
-  Singleton._() {
-    //instead of writing "_" for constructor you can write any name
+  Singleton._() { //instead of writing "_" for constructor you can write any name
     print("hello singleton");
   }
 
