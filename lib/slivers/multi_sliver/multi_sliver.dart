93189fc03a4ce4fcdb4f8950e82db13e293a98c2
Sarbad0r <avaz.shams.2002@gmail.com>
2023-08-25 07:07:13 +0500

    uploaded

--- lib/slivers/multi_sliver/multi_sliver.dart
+++ lib/slivers/multi_sliver/multi_sliver.dart
@@ -8,5 +8,6 @@ class SliverSection extends MultiSliver {
       Color headerColor = Colors.white,
       Color titleColor = Colors.black,
       required List<Widget> items})
+      //what ever you put in your children parameter would stick after each other
       : super(key: key, pushPinnedChildren: false, children: items);
 }
