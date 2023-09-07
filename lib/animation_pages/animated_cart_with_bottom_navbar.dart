04da0bd3eb5e7ef434d6f59a0b5b34b0b7bc2e7f
Sarbad0r <avaz.shams.2002@gmail.com>
2023-09-07 21:24:57 +0500

    uploaded

--- lib/animation_pages/animated_cart_with_bottom_navbar.dart
+++ lib/animation_pages/animated_cart_with_bottom_navbar.dart
@@ -18,67 +18,49 @@ class _AnimatedCartWithBottomNavbarState extends State<AnimatedCartWithBottomNav
   @override
   Widget build(BuildContext context) {
     return AddToCartAnimation(
-      // To send the library the location of the Cart icon
-      cartKey: cartKey,
-      height: 30,
-      width: 30,
-      opacity: 0.85,
-      dragAnimation: const DragToCartAnimationOptions(
-        rotation: true,
-      ),
-      jumpAnimation: const JumpAnimationOptions(),
-      createAddToCartAnimation: (runAddToCartAnimation) {
-        // You can run the animation by addToCartAnimationMethod, just pass trough the the global key of  the image as parameter
-        this.runAddToCartAnimation = runAddToCartAnimation;
-      },
-      child: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(items: [
-          const BottomNavigationBarItem(icon: Icon(Icons.add), label: '1'),
-          BottomNavigationBarItem(
-              icon: AddToCartIcon(
-                key: cartKey,
-                icon: const Icon(Icons.shopping_cart),
-                badgeOptions: const BadgeOptions(
-                  active: true,
-                  backgroundColor: Colors.red,
-                ),
-              ),
-              label: '2'),
-          const BottomNavigationBarItem(icon: Icon(Icons.add), label: '3'),
-        ]),
-        body: ListView(
-          children: List.generate(
-            15,
-            (index) {
+        // To send the library the location of the Cart icon
+        cartKey: cartKey,
+        height: 30,
+        width: 30,
+        opacity: 0.85,
+        dragAnimation: const DragToCartAnimationOptions(rotation: true),
+        jumpAnimation: const JumpAnimationOptions(),
+        createAddToCartAnimation: (runAddToCartAnimation) {
+          // You can run the animation by addToCartAnimationMethod, just pass trough the the global key of  the image as parameter
+          this.runAddToCartAnimation = runAddToCartAnimation;
+        },
+        child: Scaffold(
+            bottomNavigationBar: BottomNavigationBar(items: [
+              const BottomNavigationBarItem(icon: Icon(Icons.add), label: 'add'),
+              BottomNavigationBarItem(
+                  icon: AddToCartIcon(
+                      key: cartKey,
+                      icon: const Icon(Icons.shopping_cart),
+                      badgeOptions: const BadgeOptions(active: true, backgroundColor: Colors.red)),
+                  label: 'cart'),
+              const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home')
+            ]),
+            body: ListView(
+                children: List.generate(15, (index) {
               var widgetKey = GlobalKey();
               return InkWell(
-                onTap: () => listClick(widgetKey),
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  color: Colors.transparent,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        key: widgetKey,
-                        child: Image.network(
-                          "https://cdn.jsdelivr.net/gh/omerbyrk/add_to_cart_animation/example/assets/apple.png",
-                          width: 60,
-                          height: 60,
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Text("Widget number ${index + 1}")
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-      ),
-    );
+                  onTap: () => listClick(widgetKey),
+                  child: Container(
+                      width: 60,
+                      height: 60,
+                      color: Colors.transparent,
+                      child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                        Container(
+                            key: widgetKey,
+                            child: Image.network(
+                                "https://cdn.jsdelivr.net/gh/omerbyrk/"
+                                "add_to_cart_animation/example/assets/apple.png",
+                                width: 60,
+                                height: 60)),
+                        SizedBox(width: 10),
+                        Text("Widget number ${index + 1}")
+                      ])));
+            }))));
   }
 
   void listClick(GlobalKey widgetKey) async {
