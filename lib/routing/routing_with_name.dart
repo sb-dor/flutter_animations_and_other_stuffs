c7b15236ae0ae24d545b13ace34726b07b8e8d88
Sarbad0r <avaz.shams.2002@gmail.com>
2023-10-14 18:59:14 +0500

    uploaded

--- lib/routing/routing_with_name.dart
+++ lib/routing/routing_with_name.dart
@@ -4,10 +4,12 @@ import 'package:flutter_animations_2/material3/segment_and_badge_button_class.da
 import 'package:flutter_animations_2/nft_pages/nft_home_screen.dart';
 
 abstract class RoutingWithName {
-
   static Map<String, WidgetBuilder> routes() => {
         "/": (context) => const MainApp(),
         '/nft_home_screen': (context) => const NftHomeScreen(),
         "/segment_button_page": (context) => const SegmentButtonClass()
       };
+
+//for getting data through "pushNamed" - "arguments"
+// var any = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
 }
