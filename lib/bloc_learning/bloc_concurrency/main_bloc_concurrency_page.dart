01109e04d46f46e5fb279b893ab63807f545f175
sarbad0r <avaz.shams.2002@gmail.com>
2023-11-11 08:18:58 +0500

    added concurrency bloc

---
+++ lib/bloc_learning/bloc_concurrency/main_bloc_concurrency_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animations_2/bloc_learning/bloc_concurrency/main_bloc_concurrency.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MaiNBlocConcurrencyPage extends StatefulWidget {
+  const MaiNBlocConcurrencyPage({Key? key}) : super(key: key);
+
+  @override
+  State<MaiNBlocConcurrencyPage> createState() => _MaiNBlocConcurrencyPageState();
+}
+
+class _MaiNBlocConcurrencyPageState extends State<MaiNBlocConcurrencyPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MainBlocConcurrency, MainConcurrencyState>(builder: (context, state) {
+      var currentState = state.coutner;
+      return Scaffold(
+        appBar: AppBar(title: const Text("Main Bloc Concurrency Page")),
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    onPressed: () => context.read<MainBlocConcurrency>().add(CounterIncrement()),
+                    child: const Text("Increment")),
+                Text("$currentState"),
+                TextButton(
+                    onPressed: () => context.read<MainBlocConcurrency>().add(CounterDecrement()),
+                    child: const Text("Decrement")),
+              ]),
+        ),
+      );
+    });
+  }
+}
