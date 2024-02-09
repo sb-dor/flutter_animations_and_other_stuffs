faffa6e24e9e4c0e92e135fef95a706736132fcf
sarbador <avaz.shams.2002@gmail.com>
2024-02-09 06:24:37 +0500

    learning sockets

---
+++ lib/flutter_nearby_connectivity/yt_sockets_flutter_explained/client_socket_model.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ClientSocketModel {
+  String ipAddress;
+
+  ClientSocketModel(this.ipAddress);
+
+  Future<void> connectToTheServer() async {
+    final serverListener = await Socket.connect(ipAddress, 3000); // write same port of server
+
+    // listen all messages that coming from server
+    serverListener.listen(
+      (Uint8List data) {
+        final messageFromBytes = String.fromCharCodes(data);
+
+        debugPrint("server response: $messageFromBytes");
+      },
+      onError: (error) {
+        debugPrint("server error $error");
+        serverListener
+            .destroy(); // instead of writing "close()" method in client side serverListener should be destroyed
+      },
+      onDone: () {
+        debugPrint("server left");
+        serverListener
+            .destroy(); // instead of writing "close()" method in client side serverListener should be destroyed
+      },
+    );
+  }
+}
---
+++ lib/flutter_nearby_connectivity/yt_sockets_flutter_explained/server_socket_model.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ServerSocketModel {
+  String ipAddress;
+  List<Socket> connectedClientSocketsToServer = [];
+
+  ServerSocketModel(this.ipAddress);
+
+  Future<void> startServer() async {
+    // final ip = InternetAddress.anyIPv4; // temp
+    final server = await ServerSocket.bind(ipAddress, 3000); // whatever port you want
+
+    server.listen(
+      (socket) {
+        _handleConnection(socket);
+      },
+      onError: () {
+        debugPrint("error connection");
+      },
+      onDone: () {},
+    );
+  }
+
+  void _handleConnection(Socket clientSocket) {
+    clientSocket.write("Success fully joined to the server"); // writing to client from server
+
+    clientSocket.listen(
+      (Uint8List data) {
+        // coming information from client
+
+        String messageFromBytes = String.fromCharCodes(data);
+
+        for (final each in connectedClientSocketsToServer) {
+          each.write("Someone wrote: $messageFromBytes");
+        }
+
+        if (!connectedClientSocketsToServer.any((el) => el.address == clientSocket.address)) {
+          connectedClientSocketsToServer.add(clientSocket);
+        }
+
+        clientSocket.write("Server: you are writing : $messageFromBytes");
+      },
+      onError: (error) {
+        debugPrint("client error connection $error");
+        clientSocket.close();
+      },
+      onDone: () {
+        debugPrint("client left the server");
+        clientSocket.close();
+      },
+    );
+  }
+}
