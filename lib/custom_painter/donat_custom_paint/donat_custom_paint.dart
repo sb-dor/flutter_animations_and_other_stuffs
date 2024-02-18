1b912a32f7572588550be4a71fe3aa842b939025
sarbador <avaz.shams.2002@gmail.com>
2024-02-18 16:28:19 +0500

    custom_painters_learning

---
+++ lib/custom_painter/donat_custom_paint/donat_custom_paint.dart
@@ -0,0 +1,160 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class DataItem {
+  final double value;
+  final String label;
+  final Color color;
+
+  late AnimationController _animationController;
+  late Animation<double> animation;
+
+  DataItem({
+    required this.value,
+    required this.label,
+    required this.color,
+    required TickerProvider provider,
+  }) {
+    _animationController =
+        AnimationController(vsync: provider, duration: const Duration(seconds: 2));
+    animation = Tween<double>(begin: 0, end: value)
+        .animate(CurvedAnimation(parent: _animationController, curve: Curves.fastOutSlowIn));
+    _animationController.forward();
+  }
+}
+
+class DonalCustomPaint extends StatefulWidget {
+  const DonalCustomPaint({super.key});
+
+  @override
+  State<DonalCustomPaint> createState() => _DonalCustomPaintState();
+}
+
+class _DonalCustomPaintState extends State<DonalCustomPaint> with TickerProviderStateMixin {
+  List<AnimationController> animationControllers = [];
+
+  List<DataItem> dataItems = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    dataItems = [
+      DataItem(value: 0.2, label: "Comedy", color: Colors.red, provider: this),
+      DataItem(value: 0.25, label: "Action", color: Colors.amber, provider: this),
+      DataItem(value: 0.3, label: "Romance", color: Colors.blue, provider: this),
+      DataItem(value: 0.05, label: "Drama", color: Colors.green, provider: this),
+      DataItem(value: 0.2, label: "SciFi", color: Colors.orange, provider: this),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomPaint(
+              child: SizedBox(
+                width: 200,
+                height: 200,
+              ),
+              painter: DonatChartPainter(dataItems),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DonatChartPainter extends CustomPainter {
+  List<DataItem> items;
+
+  DonatChartPainter(this.items) : super(repaint: items.first.animation);
+
+  final Paint whitePainterInCircle = Paint()
+    ..color = Colors.white
+    ..strokeWidth = 2;
+
+  final midPainter = Paint()
+    ..color = Colors.white
+    ..style = PaintingStyle.fill;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final radius = size.width * 0.9;
+
+    final rect = Rect.fromCenter(center: center, width: radius, height: radius);
+
+    var startAngle = 0.0;
+
+    for (var each in items) {
+      // drawing arcs
+      final paint = Paint()
+        ..color = each.color
+        ..style = PaintingStyle.fill;
+
+      final sweepAngle = each.animation.value * 360 * pi / 180;
+
+      canvas.drawArc(rect, startAngle, sweepAngle, true, paint);
+      //
+
+      //drawing lines
+      final dx = radius / 2 * cos(startAngle);
+
+      final dy = radius / 2 * sin(startAngle);
+
+      final p2 = center + Offset(dx, dy);
+
+      canvas.drawLine(center, p2, whitePainterInCircle);
+      //
+
+      final r = radius * 0.4;
+      final x = r * cos(startAngle + sweepAngle / 2);
+      final y = r * sin(startAngle + sweepAngle / 2);
+      final pos = center + Offset(x, y);
+
+      drawTextCentered(
+        canvas,
+        pos,
+        each.label,
+        TextStyle(color: Colors.black, fontSize: 10),
+        radius * 0.3,
+      );
+
+      startAngle += sweepAngle;
+    }
+
+    // draw middle white circle
+    canvas.drawCircle(center, radius * 0.3, midPainter);
+
+    // draw title
+    drawTextCentered(canvas, center, "Favorite dates",
+        const TextStyle(color: Colors.black, fontSize: 15), radius * 0.6);
+  }
+
+  TextPainter measureText(String s, TextStyle textStyle, double maxWidth, TextAlign textAlign) {
+    final span = TextSpan(text: s, style: textStyle);
+    final tp = TextPainter(text: span, textAlign: textAlign, textDirection: TextDirection.ltr);
+    tp.layout(minWidth: 0, maxWidth: maxWidth);
+    return tp;
+  }
+
+  Size drawTextCentered(
+      Canvas canvas, Offset position, String text, TextStyle textStyle, double maxWidth) {
+    final tp = measureText(text, textStyle, maxWidth, TextAlign.center);
+    final pos = position + Offset(-tp.width / 2, -tp.height / 2);
+    tp.paint(canvas, pos);
+    return tp.size;
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
---
+++ lib/custom_painter/draw_vertices/draw_vertices_page.dart
@@ -0,0 +1,140 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class DrawVerticesPage extends StatefulWidget {
+  const DrawVerticesPage({super.key});
+
+  @override
+  State<DrawVerticesPage> createState() => _DrawVerticesPageState();
+}
+
+class _DrawVerticesPageState extends State<DrawVerticesPage> with SingleTickerProviderStateMixin {
+  late AnimationController _rotationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _rotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 5).animate(
+      CurvedAnimation(
+        parent: _rotationController,
+        curve: Easing.legacy,
+      ),
+    );
+
+    _rotationController.forward();
+
+    _rotationController.addListener(() {
+      if (_rotationController.isCompleted) {
+        _rotationController.reverse();
+      }
+      if (_rotationController.value == 0) {
+        _rotationController.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _rotationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: AngleAnimation(_animation),
+        child: const SizedBox.expand(),
+      ),
+    );
+  }
+}
+
+class AngleAnimation extends CustomPainter {
+  final Animation<double> animation;
+
+  AngleAnimation(this.animation) : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final paint = Paint()
+      ..color = Colors.amber
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 5;
+
+    final points1 = [
+      center + Offset(0, -animation.value),
+      center + Offset(50 + animation.value, animation.value),
+      center + Offset(50 + animation.value, 50 + animation.value),
+      center + Offset(0, 50 - animation.value),
+      center + Offset(0, -animation.value)
+    ];
+
+    // final vertices = Vertices(
+    //   VertexMode.triangles,
+    //   [
+    //     center,
+    //     center + Offset(50, 0),
+    //     center + Offset(0, 30),
+    //     center + Offset(50, 30),
+    //   ],
+    // );
+
+    canvas.drawPoints(PointMode.polygon, points1, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
+
+class ExamplePainter extends CustomPainter {
+  final Animation<double> rotation;
+
+  ExamplePainter(this.rotation) : super(repaint: rotation);
+
+  final Paint _paint = Paint()..color = const Color(0xFFFF33FF);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final radius = size.shortestSide / 3;
+
+    final center = Offset(size.width / 2, size.height / 2);
+
+    final Vertices vertices = Vertices(
+      VertexMode.triangles,
+      [
+        center,
+        center + const Offset(100, 0),
+        center + const Offset(100, 100),
+        center + Offset(0, 100),
+      ],
+      colors: [
+        Colors.red,
+        Colors.green,
+        Colors.blue,
+        Colors.black,
+      ],
+      // index of positions from top that will be used
+      // ( you can use every of them with no limits) only if it create triangle
+      indices: [0, 2, 3, 1, 2, 3],
+    );
+
+    canvas.drawVertices(vertices, BlendMode.dst, _paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
