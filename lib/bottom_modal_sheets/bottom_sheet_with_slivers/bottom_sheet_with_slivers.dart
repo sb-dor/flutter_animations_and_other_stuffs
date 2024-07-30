0c482d7b82f71acc15ff3f76ed1b7c766a7511f5
sb-dor <avaz.shams.2002@gmail.com>
2024-07-30 22:01:47 +0500

    writing similar code for double modal bottom sheet

--- lib/bottom_modal_sheets/bottom_sheet_with_slivers/bottom_sheet_with_slivers.dart
+++ lib/bottom_modal_sheets/bottom_sheet_with_slivers/bottom_sheet_with_slivers.dart
@@ -18,7 +18,8 @@ class _BottomSheetWithSliversState extends State<BottomSheetWithSlivers> {
             showModalBottomSheet(
               context: context,
               isScrollControlled: true,
-              builder: (context) => const _MyModalSheet(),
+              builder: (context) => const ClipRRect(child: _MyModalSheet()),
+              shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
             );
           },
           child: const Text('Show Modal Bottom Sheet'),
@@ -41,21 +42,36 @@ class _MyModalSheet extends StatelessWidget {
           controller: scrollController,
           slivers: [
             // SliverPersistentHeader for the images at the top
-            SliverPersistentHeader(
-              delegate: MySliverAppBar(
-                expandedHeight: 200.0,
+            // SliverPersistentHeader(
+            //   delegate: MySliverAppBar(
+            //     expandedHeight: 200.0,
+            //   ),
+            //   pinned: false,
+            //   floating: true,
+            //   // floating: true,
+            //   // pinned: false,
+            // ),
+            SliverAppBar(
+              scrolledUnderElevation: 0,
+              expandedHeight: 200,
+              leading: SizedBox(),
+              flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.none,
+                background: Image.asset(
+                  'assets/parallax_effect_images/efe-kurnaz.jpg',
+                  fit: BoxFit.none,
+                ),
               ),
-              pinned: true,
             ),
             // SliverList for the scrollable list items
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                  return ListTile(
-                    title: Text('Item $index'),
-                  );
+            SliverToBoxAdapter(
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text('Item $index'));
                 },
-                childCount: 20,
               ),
             ),
           ],
@@ -71,34 +87,51 @@ class MySliverAppBar extends SliverPersistentHeaderDelegate {
   MySliverAppBar({required this.expandedHeight});
 
   @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Stack(
+      clipBehavior: Clip.none,
       fit: StackFit.expand,
       children: [
         // Background images
-        Image.asset('assets/image1.jpg', fit: BoxFit.cover),
-        Positioned(
-          top: 0,
-          child: Image.asset('assets/image2.jpg', fit: BoxFit.cover),
+        Image.asset(
+          'assets/parallax_effect_images/efe-kurnaz.jpg',
+          fit: BoxFit.none,
         ),
         Positioned(
-          top: 0,
-          child: Image.asset('assets/image3.jpg', fit: BoxFit.cover),
-        ),
-        // Overlay content
-        Positioned(
-          bottom: 16.0,
-          left: 16.0,
-          child: Text(
-            'Header Content',
-            style: TextStyle(
-              fontSize: 32.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+          bottom: -0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 20,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                )),
           ),
-        ),
+        )
+        // Positioned(
+        //   top: 0,
+        //   child: Image.asset('assets/image2.jpg', fit: BoxFit.cover),
+        // ),
+        // Positioned(
+        //   top: 0,
+        //   child: Image.asset('assets/image3.jpg', fit: BoxFit.cover),
+        // ),
+        // Overlay content
+        // Positioned(
+        //   bottom: 16.0,
+        //   left: 16.0,
+        //   child: Text(
+        //     'Header Content',
+        //     style: TextStyle(
+        //       fontSize: 32.0,
+        //       color: Colors.white,
+        //       fontWeight: FontWeight.bold,
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
