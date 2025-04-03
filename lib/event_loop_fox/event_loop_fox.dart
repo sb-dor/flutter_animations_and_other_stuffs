6d84cee9ef766c701c2e1904b18c84e52371f322
sb-dor <avaz.shams.2002@gmail.com>
2025-04-03 06:16:32 +0500

    added some notes

--- lib/event_loop_fox/event_loop_fox.dart
+++ lib/event_loop_fox/event_loop_fox.dart
@@ -1,7 +1,9 @@
 // just imagine that Event loop is just a person that is alive throughout you app
 // and does some tasks, for ex: He has to jobs, 1) He has to check all events that comes the
-// "Event queue" (place where functions come, like:) and to the "Micro task queue" (place where simple functions come, like)
-//
+// "Event queue" (place where functions come, like: Future.microtask((){}) ) and to the "Micro task queue" (place where simple functions come, like: Functions, Futures)
+// This event loop runs inside one Isolate, Remember that each isolate has it's own "Event loop"
+import 'dart:async';
+
 class EventLoopTest {
   EventLoopTest(this.eventQueue, this.microTaskQueue);
 
@@ -9,6 +11,8 @@ class EventLoopTest {
   final List<void Function()> microTaskQueue;
 
   void handleEventLoop() {
+    // while app is running (until it finishes), event loop will work
+    // when the microTaskQueue and eventQueue are empty, isolate finishes
     while (true) {
       if (microTaskQueue.isNotEmpty) {
         for (final each in microTaskQueue) {
@@ -23,4 +27,11 @@ class EventLoopTest {
   }
 }
 
-void main() {}
+void main() {
+  // 1) All events that we do in our app like: Functions, Future Functions,
+  // All that works go the "Event Queue" automatically
+  // 2) In order to add microTask inside "MicroTask Queue" you have to use "Future.microtask((){})"
+
+  scheduleMicrotask(() {});
+  Future.microtask(() {}); // shortcut for "scheduleMicrotask"
+}
